6a3bb978910ebf7db36513a3b864668e50fec4eb
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-15 16:22:12 -0400

    Adding an onChange to the Field.js file and Contact.js file

--- portfolio/src/Pages/Contact/Contact .js
+++ portfolio/src/Pages/Contact/Contact .js
@@ -13,27 +13,35 @@ class Contact extends Component {
             message: "",
         };
         ///Going to use a a hook to ensure this, when calling 'this.updateField' refers to Contact nand not Field////
-        
+        this.updateField = this.updateField.bind(this);
     }
     updateField(field,value) {
+        this.setState({[ field]: value});
+        
+    
 
     }
     render() {
         return (
             <div>
-                <Navbar>
+                <Navbar />
+                <h1>Contact</h1>
                  {/*Name Field */}
-                <Field onChange={(event) => this.updateField('name', event.target.value)} value={this.state.name} />
+                <Field onChange={(event) => this.updateField('name', event.target.value)} 
+                onFocus={this.state.updateField}
+                value={this.state.name} />
                 {/*Email Field */}
                 
-                <Field onChange={(event) => this.updateField('email', event.target.value)} value={this.state.email} />
+                <Field onChange={(event) => this.updateField('email', event.target.value)}
+                 value={this.state.email} />
                 {/*message textarea */}
                 
-                <Field textarea onChange={(event) => this.updateField('message', event.target.value)} value={this.state.message} />
+                <Field onChange={(event) => this.updateField('message', event.target.value)}
+                textarea={true}
+                value={this.state.message} />
                 
                 {/*Submit Button */}
                 <Button formValues={this.state} email="joshuaalabre@gmail.com" />
-                </Navbar>
             </div>
         )
     }
