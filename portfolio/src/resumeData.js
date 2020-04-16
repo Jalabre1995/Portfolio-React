5faf2f795edba2328954f3fa5b08a29c4f17767f
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-16 16:44:53 -0400

    Adding an item-list for Portfolio.js and creating resumeData

---
+++ portfolio/src/resumeData.js
@@ -0,0 +1,123 @@
+let resumeData = {
+    "name": 'Joshua Alabre',
+    'role': 'Full Stack Deeloper',
+    'linkedinID': 'Your LinkedIn Id',
+    'roleDescription': 'Multidisciplinary Full Stack Developer with a passion for condensing large data into a simple to understand format. And build websites from mockup to rollout. Adept at working independently as well as collaborating with teams across multiple functions to break down concepts in easy terms.Prepared to excel in solving complex problems.',
+    'socialLinks': [
+        {
+            "name":'linkedin',
+            'url': 'https://www.linkedin.com/in/joshua-alabre-6a628a10b/'
+            
+        },
+        {
+            'name': 'github',
+            'url': 'https://github.com/Jalabre1995',
+        }
+    ],
+    'education': [
+        {
+            'UniversityName': 'Wheaton College',
+            'speacialization': 'Some spcialization',
+            'StartDate': 'Aug',
+            'YearOfPassing': '2018',
+            'Achievemnts': 'Some Achievements'
+        },
+        {
+            'UniersityName': 'UCONN',  
+            'speacilization': 'Coding/FullStack',
+            'MonthOfLeaving': 'May',
+            'YearOfPassing': '2020',
+            'Achievements': 'Learned Index.html, CSS, Javascript, Jquery, IndexDB, Mongoose, MongoDB, React, Redux, and Handlebars'
+        }
+        
+
+    ],
+    'work': [
+        {
+            'CompanyName': 'Fleet Feet Sports',
+            'specilization': 'Sales-Represenative',
+            'MonthOfLeaving': 'Current-Employer',
+            'YearOfLeaving': 'Current-Employer',
+        },
+        {
+            'CompanyName': 'Energybits',
+            'specialization': 'Sales-Represenative',
+            'MonthOfLeaving': 'August',
+            'YearOfLeaving': '2017',
+        }
+    ],
+    'skillsDescription': 'Your skills here',
+    'skills': [
+        {
+            'skillname': 'HTML5'
+        },
+        {
+            'skillname': 'CSS'
+        },
+        {
+            'skillname': 'Javascript'
+        },
+        {
+            'skillname': 'Jquery'
+        },
+        {
+            'skillname': 'IndexDB'
+        },
+        {
+            'skillname': 'MongoDB'
+        },
+        {
+            'skillname': 'Mongoose'
+        },
+        {
+            'skillname': "MYSQL"
+        },
+        {
+            'skillname': 'React'
+        }
+    ],
+    'portfolio': [
+        {
+            'name': 'project1',
+            'description': 'Gaming-API',
+            "imgurl": 'images/Game-API.jpg'
+        },
+        {
+            'name': 'project2',
+            'description': 'NBA-Game',
+            'imgurl': 'images/NBA.jpg'
+        },
+        {
+            'name':'project3',
+            'description':'Day-Planner',
+            'imgurl': 'images/Day-Planner.png' 
+        },
+        {
+            'name': 'project4',
+            'description': 'exmployee-tracker',
+            'imgurl': 'images/employee-tracker.png'
+
+        },
+        {
+            'name': 'project5',
+            'description': 'Java-test',
+            'imgurl': 'images/Java-test.jpg'
+        },
+
+        {
+            'name': 'project6',
+            'description': 'Password',
+            'imgurl': 'images/Password.jpg'
+        },
+        {
+            'name': 'project7',
+            'description': 'Rock-paper-Scissors',
+            'imgurl': 'images/Rock-paper-Scissors.jpg'
+        }
+      
+    ]
+
+
+}
+
+export default resumeData;
