bc459d4b91b3b920cfd08de4d0c51c037f97478f
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-16 13:33:11 -0400

    Adding images tot he portfolio.js file

---
+++ portfolio/src/Pages/Portfolio/Portfolio.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+export default class Portfolio extends Component {
+    render() {
+        let resumeData = this.props.resumeData;
+        return (
+            <section id='portfolio'>
+                <div className='row'>
+                    <div className='twelve colums collapsed'>
+                        <h1> Check Out Some of the Work I Did!</h1>
+                        <div id='portfolio-wrapper' className='bgrid-quarters s-bgrid-thirds cf'>
+                            {
+                                resumeData.portfolio && resumeData.portfolio.map((item) => {
+                                    return (
+                                        <div className='columns portfolio-item'>
+                                            <div className='item-wrap'>
+                                                <a href='https://github.com/justindurocher/Video-Game-Search-App'>
+                                                    <img src='' className='GameAPI' /> 
+                                                    <div className='overlay'>
+                                                        <div className='portfolio-item-meta'>
+                                                            <h5>{item.name}</h5>
+                                                            <p>{item.description}</p>
+
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
