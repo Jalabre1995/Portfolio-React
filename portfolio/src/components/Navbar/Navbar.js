7e5e99ac8794e78c87bd57a090dfe605fe087ecd
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-16 17:13:39 -0400

    Updated Navbar to the links for About, Resume, Contacts, and Home

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import {Link} from 'react=router-dom ';
-import './style.css';
-
-function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        About 
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/portfolio" || window.location.pathname === "/portfolio"
-                  ? "nav-link active"
-                  : "nav-link"
+import React, {Component} from 'react';
+export default class Navbar extends Component{
+render(){
+  let resumeData = this.props.resumeData;
+  return(
+    <React.Fragment>
+      <header id='home'>
+        <nav id='nav-wrap'>
+          <a className='nav-btn' href= '#nav-wrap' title= 'Show Navigation'>Show naigation</a>
+          <ul id ='nav'className='nav'>
+            <li className='current'><a className='smoothscroll'href='home'>Home</a></li>
+            <li><a className='smoothscroll'href='#about'>About</a></li>
+            <li><a className='smoothscroll'href='#resume'>Resume</a></li>
+            <li><a className='smoothscroll'href='#contact'>Contact</a></li>
+            <li><a className='smoothscroll'href='#portfolio'>Portfolio</a></li>
+          </ul>
+        </nav>
+        <div className='row banner'>
+          <div className='banner-text'>
+            <h1 className='headline'>I am {resumeData.name}.</h1>
+            <h3 style={{color:'#fff', fontFamily:'sans=serif'}}>I am a {resumeData.role}.{resumeData.roleDescription}
+            </h3>
+            <br/>
+            <ul className='social'>
+              {
+                resumeData.socialLinks && resumeData.socialLink.map(item => {
+                  return(
+                    <li key={item.name}>
+                      <a href={item.url} target='__blank'><li className= {item.className}></li></a>
+                    </li>
+                  )
+                })
               }
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/contact"
-              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    )
+            </ul>
+          </div>
+        </div>
+      </header>
+    </React.Fragment>
+  )
 }
-
-
-export default Navbar;
+}
