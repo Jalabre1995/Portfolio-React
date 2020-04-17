61e526c1507e03c90ebd47099c0dc0024002dba4
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-17 13:04:16 -0400

    Adding react-router-dom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
-import Navbar from './components/Navbar/Navbar.js';
-import About from './Pages/About/About.js';
-import resumeData from './resumeData.js';
-import Portfolio from './Pages/Portfolio/Portfolio.js';
-import Contact from './Pages/Contact/Contact .js';
-import Footer from './components/Footer/Footer.js';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl'
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <Navbar resumeData={resumeData}/>
-        <About resumeData={resumeData} />
-        <Portfolio resumeData={resumeData} />
-        <Contact resumeData={resumeData} />
-        <Footer resumeData={resumeData} />
-      </div>
+      <div className="demo-big-content">
+      <Layout>
+          <Header title="Title" scroll>
+              <Navigation>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+              </Navigation>
+          </Header>
+          <Drawer title="Title">
+              <Navigation>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+              </Navigation>
+          </Drawer>
+          <Content>
+              <div className="page-content" />
+          </Content>
+      </Layout>
+  </div>
     );
   }
   
