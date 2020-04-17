65e89e936ecac960083120b0db82824b400b2df4
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-17 11:39:14 -0400

    Changes to the contact.js file

--- portfolio/src/Pages/Contact/Contact .js
+++ portfolio/src/Pages/Contact/Contact .js
@@ -1,49 +1,26 @@
 import React, { Component } from 'react';
-
-import Navbar from "../../components/Navbar.js";
-import Field from "../../components/Field.js";
-import Button from "../../components/Button.js";
-
-class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "",
-            email: "",
-            message: "",
-        };
-        ///Going to use a a hook to ensure this, when calling 'this.updateField' refers to Contact nand not Field////
-        this.updateField = this.updateField.bind(this);
-    }
-    updateField(field,value) {
-        this.setState({[ field]: value});
-        
-    
-
-    }
-    render() {
+export default class Contact extends Component{
+    render(){
+        let resumeData = this.props.resumeData;
         return (
-            <div>
-                <Navbar />
-                <h1>Contact</h1>
-                 {/*Name Field */}
-                <Field onChange={(event) => this.updateField('name', event.target.value)} 
-                onFocus={this.state.updateField}
-                value={this.state.name} />
-                {/*Email Field */}
-                
-                <Field onChange={(event) => this.updateField('email', event.target.value)}
-                 value={this.state.email} />
-                {/*message textarea */}
-                
-                <Field onChange={(event) => this.updateField('message', event.target.value)}
-                textarea={true}
-                value={this.state.message} />
-                
-                {/*Submit Button */}
-                <Button formValues={this.state} email="joshuaalabre@gmail.com" />
-            </div>
+            <section id='contact'>
+                <div className='row section-head'>
+                    <div className= 'ten columns'>
+                        <p className='lead'>
+                            Feel free to contact me for any project suggestions.
+                        </p>
+                    </div>
+                </div>
+                <div className='row'>
+                    <aside className='eight columns footer-widgets'>
+                        <div className='widget'>
+                            <h4> Linked in :
+                                {resumeData.linkedId}
+                            </h4>
+                        </div>
+                    </aside>
+                </div>
+            </section>
         )
     }
 }
-export default Contact;
